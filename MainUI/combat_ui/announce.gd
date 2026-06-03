8abcb302ac69e729119ce7df8f22c7d2524820c1
RyuzWha <knextank@naver.com>
2026-06-04 00:37:46 +0900

    savebeforeinputrefactor

--- MainUI/combat_ui/announce.gd
+++ MainUI/combat_ui/announce.gd
@@ -2,6 +2,13 @@ extends Label
 
 @export var max_length : int = 4
 var combat_log_list : Array[String]
+var drag_start_pos : Vector2
+var drag_end_pos : Vector2
+var is_dragging : bool:
+	set(new_value):
+		if is_dragging == true and new_value == false and (drag_start_pos.x - drag_end_pos.x) >= 100:
+			print("dragged draggeddraggeddraggeddraggeddraggeddraggeddraggeddraggeddraggeddraggeddraggeddragged")
+
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	GlobalSignal.announced.connect(announce)
@@ -11,3 +18,16 @@ func announce(announcement : String):
 	if combat_log_list.size() > max_length:
 		combat_log_list.pop_back()
 	text = "\n".join(combat_log_list)
+
+
+func _process(_delta: float) -> void:
+	if Input.is_action_pressed("click"):
+		is_dragging = true
+		print(self, " dragging started")
+		drag_start_pos = get_global_mouse_position()
+		
+	if Input.is_action_just_released("click"):
+		is_dragging = false
+		print(self, " dragging ended")
+		drag_end_pos = get_global_mouse_position()
+		
--- MainUI/combat_ui/damage_number/damage_number_zone.gd
+++ MainUI/combat_ui/damage_number/damage_number_zone.gd
@@ -4,14 +4,17 @@ extends Control
 @onready var rect: Control = self
 @onready var damage_number: DamageNumber = $DamageNumber
 
-
+var drag_start_pos : Vector2
+var drag_end_pos : Vector2
+var is_dragging : bool
 
 func _ready() -> void:
-	print(self, " in tree")
-	for i in 4:
-		show_damage_number()
-		await get_tree().create_timer(0.3).timeout
-
+	##print(self, " in tree")
+	##for i in 4:
+	##	show_damage_number()
+	##	await get_tree().create_timer(0.3).timeout
+	return
+	
 func show_damage_number():
 	var new_damage_number = damage_number.duplicate()
 	new_damage_number.global_position = get_random_global_position()
